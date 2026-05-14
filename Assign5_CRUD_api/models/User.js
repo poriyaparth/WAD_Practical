400bb0a0ad4f8ed7d791298ae5fc965ed33fab42
Parth Poriya <166291674+poriyaparth@users.noreply.github.com>
2026-05-14 12:26:38 +0530

    Update User.js

--- Assign5_CRUD_api/models/User.js
+++ Assign5_CRUD_api/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    city: String
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    city: { type: String, required: true }
 });
 
 module.exports = mongoose.model("User", userSchema);
